47b1172eb3c62e65eb28ac0938bcb1b96e1e8bde
Trisha Langlois <46874658+trishalanglois@users.noreply.github.com>
2019-12-03 14:14:43 -0700

    Add fetching for homeworld

--- src/App.js
+++ src/App.js
@@ -12,19 +12,58 @@ class App extends Component {
         rank: '',
         favoriteCharacters: []
       },
-      movies: {
-        title: '',
-        episode: null,
-        releaseDate: null,
-        characters: [],
-        image: '../images/movie.jpeg'
-      }
+      movies: []
+      // {
+      //   title: '',
+      //   episode: null,
+      //   releaseDate: null,
+      //   characters: [],
+      //   image: '../images/movie.jpeg',
+      //   openingCredits: ''
+      // }
     }
   }
 
   componentDidMount() {
     fetch('https://swapi.co/api/films/')
-  }
+      .then(response => response.json())
+      .then(data => {
+        let moviesAndCharactersPromises = data.results.map(movie => {
+
+          return movie.characters.map(character => {
+            let world = fetchWorld(character);
+            return fetch(character)
+            .then(response => response.json())
+            .then(character => ({
+              name: character.name,
+              world: world.name,
+              population: world.population,
+              species: //fetch,
+              relatedFilms: //fetch
+            }))
+          }
+          ))
+
+          return {
+            title: movie.title,
+            episode: movie.episode_id,
+            releaseDate: release_date,
+
+          }
+        })
+      })
+    }
+
+    fetchWorld(character) {
+      let homeworld =
+      fetch(character.homeworld)
+        .then(response => response.json())
+        .then(data => data)
+      return {
+        name: homeworld.name,
+        population: homeworld.population
+      }
+    }
 
   render() {
     return (
