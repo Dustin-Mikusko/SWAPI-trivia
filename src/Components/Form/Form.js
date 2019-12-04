c8f6f608175df17cd7f64398fd1a46e4d0c22617
Dustin Mikusko <dustinjm713@yahoo.com>
2019-12-04 14:46:06 -0700

    Add handleChange method, checkInputs method, and clearInputs method to Form component

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -13,25 +13,50 @@ class Form extends Component {
     }
   }
 
+  handleChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  }
+
+  clearInputs = () => {
+    this.setState({ name: '', quote: '', rank: '' });
+  }
+
+  checkInputs = () => {
+    Object.keys(this.state).forEach(key => {
+      if (!this.state[key]) {
+        document.querySelector(`.${key}`).classList.add('error-border')
+      } else {
+        document.querySelector(`.${key}`).classList.remove('error-border')
+      }
+    })
+  }
+
+  // goToNext() {
+  //   this.checkInputs();
+  // }
+
   render() {
     return (
       <form>
       <img src={starWarsLogo} alt='Star Wars logo' />
        <input
+          className="name"
           type="text"
           placeholder=" Name"
           name="name"
           value={this.state.name}
-          onChange={event => this.handeChange(event)}
+          onChange={event => this.handleChange(event)}
         />
         <input
+          className="quote"
           type="text"
           placeholder=" Favorite Star Wars Quote"
           name="quote"
           value={this.state.quote}
-          onChange={event => this.handeChange(event)}
+          onChange={event => this.handleChange(event)}
         />
         <select
+          className="rank"
           name="rank"
           value={this.state.rank}
           onChange={event => this.handleChange(event)}
@@ -41,7 +66,7 @@ class Form extends Component {
             <option value="intermediate">Padawan</option>
             <option value="expert">The Force is Strong With This One...</option>
         </select>
-        <button type="button" onClick={this.goToNext}>Enter, You Will</button>
+        <button type="button" onClick={this.checkInputs}>Enter, You Will</button>
       </form>
     )
   }
