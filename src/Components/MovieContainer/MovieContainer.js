d09bf0d40c26db1547e994cecd19ca62f95b841a
Dustin Mikusko <dustinjm713@yahoo.com>
2019-12-06 16:53:07 -0700

    Render movie container

--- src/Components/MovieContainer/MovieContainer.js
+++ src/Components/MovieContainer/MovieContainer.js
@@ -5,8 +5,7 @@ import Header from '../Header/Header'
 
 const MovieContainer = ({ movies }) => {
   const allMovies = movies.map((movie, i) => {
-    return (
-      < Movie
+    return < Movie
         key = {i}
         title = {movie.title}
         episode = {movie.episode}
@@ -14,16 +13,12 @@ const MovieContainer = ({ movies }) => {
         image = {movie.image}
         openingCredits = {movie.openingCredits}
       />
-    )
   })
 
   return (
-    <body>
-      <Header />
       <main>
           {allMovies}
       </main>
-    </body>
   )
 }
 
