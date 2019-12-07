09c3e963473e3d2309330f2ac5f89ecf57f92501
Dustin Mikusko <dustinjm713@yahoo.com>
2019-12-06 17:00:52 -0700

    Render movie specific datafor each movie card

--- src/Components/Movie/Movie.js
+++ src/Components/Movie/Movie.js
@@ -3,14 +3,14 @@ import './Movie.css';
 import PropTypes from 'prop-types';
 import poster from '../../images/movie.jpeg';
 
-const Movie = ({ title, releaseDate, episodeId }) => {
+const Movie = ({ title, releaseDate, episode }) => {
   return (
     <article>
       <div className="image">
-      <h1>EPISODE I</h1>
+      <h1>EPISODE {episode}</h1>
       </div>
-      <h3>Title: <span>Revenge of the Sith</span></h3>
-      <h3>Release Date: <span>2005</span></h3>
+      <h3>Title: <span>{title}</span></h3>
+      <h3>Release Date: <span>{releaseDate}</span></h3>
       <button type="button">View Characters</button>
     </article>
   )
