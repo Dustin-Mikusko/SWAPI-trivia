75a848795aaa40aa7ed91d0c94df1e7f22640815
Trisha Langlois <46874658+trishalanglois@users.noreply.github.com>
2019-12-06 21:29:23 -0700

    Add font styling to username and glowing bottom border to header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import './Header.css';
 import babyYoda from '../../images/baby-yoda.jpeg';
-
+import lightsaberLogout from '../../images/light-saber-x.jpeg';
 
 
 const Header = ({ user }) => {
   return (
     <header>
-      <div className='user-badge'>
-        <h2>{user.name}</h2>
-        <img className='baby-yoda' src={babyYoda} />
-        <p>{user.rank}</p>
+      <div className='header-container'>
+        <div className='user-badge'>
+          <h2>{user.name}</h2>
+          <img className='baby-yoda' src={babyYoda} />
+          <p>{user.rank}</p>
+        </div>
+        <h2 className='header-quote'>{user.quote}</h2>
+        <img className='logout-button' src={lightsaberLogout} />
       </div>
-      <h2 className='header-quote'>{user.quote}</h2>
-      <button className='logout-button' type= "button">Log Out</button>
     </header>
   )
 }
