8b6c4458fb71e2af3c5dd9b88152c7bb7182e508
Dustin Mikusko <dustinjm713@yahoo.com>
2019-12-08 10:26:36 -0700

    Write skeleton for CharacterContainer

---
+++ src/Components/CharacterContainer/CharacterContainer.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './CharacterContainer.css';
+import Character from '../Character/Character';
+
+const CharacterContainer = ({ movie }) => {
+  console.log(movie.characters);
+  const characters = movie.characters.map(character => {
+    console.log(character.species);
+    console.log(character.world)
+    return <Character 
+      name={character.name}
+      // world={character.world.name}
+      // population={character.world.population}
+      // species={character.species[0]}
+      // relatedFilms={character.relatedFilms.join(', ')}
+    />
+  })
+
+  return (
+    <>
+     {characters}
+    </>
+  )
+}
+
+export default CharacterContainer;
