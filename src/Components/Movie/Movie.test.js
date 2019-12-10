6c35e8f481677ca3ad5430e2e2734709e0f82e71
Dustin Mikusko <dustinjm713@yahoo.com>
2019-12-09 17:44:43 -0700

    Finish testing for Movie

---
+++ src/Components/Movie/Movie.test.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Movie from './Movie';
+
+describe('Movie', () => {
+  const mockMovie = {
+    key: 1,
+    title: "Revenge of the Sith",
+    episode: "4",
+    releaseDate: "2002",
+    image: "url.to.movie",
+    openingCredits: "WeeWooWeeWoo",
+    characters: [{name: 'Tom Brady'}],
+    checkMovies: jest.fn()
+  };
+
+  let wrapper = shallow(<Movie 
+    key={mockMovie.key}
+    title={mockMovie.title}
+    episode={mockMovie.episode}
+    releaseDate={mockMovie.releaseDate}
+    image={mockMovie.image}
+    openingCredits={mockMovie.openingCredits}
+    characters={mockMovie.characters}
+  />)
+
+  it('should render the component with the correct data to match snapshot', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should call checkMovies when the Link tag is clicked', () => {
+    const mockCheckMovies = jest.fn();
+    wrapper = shallow(<Movie 
+      key={mockMovie.key}
+      title={mockMovie.title}
+      episode={mockMovie.episode}
+      releaseDate={mockMovie.releaseDate}
+      image={mockMovie.image}
+      openingCredits={mockMovie.openingCredits}
+      characters={mockMovie.characters}
+      checkMovies={mockCheckMovies}
+    />)
+
+    wrapper.find('Link').simulate('click');
+
+    expect(mockCheckMovies).toHaveBeenCalled();
+  })
+})
