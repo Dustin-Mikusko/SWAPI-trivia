ccdd363593a4cd8e874b1ec671d81d3118e0d58f
Trisha Langlois <46874658+trishalanglois@users.noreply.github.com>
2019-12-10 14:54:48 -0700

    Add image file to character card

--- src/Components/Character/Character.js
+++ src/Components/Character/Character.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './Character.css';
+import characterImage from '../../images/character-picture.jpeg'
 
 const Character = ({ name, world, population, species, relatedFilms }) => {
   return (
     <section className="character-cards">
       <article className="character-card">
-        <h1 className="character-name">{name}</h1>
+        <img src={characterImage} alt="image of two star wars silhouettes battling" height="30" width="30">
+        <h4 className="character-name">{name}</h1>
         <p><span className="bold">World:</span> {world}</p>
         <p><span className="bold">Population:</span> {population}</p>
         <p><span className="bold">Species:</span> {species}</p>
